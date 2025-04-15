1b0b83a2eaf8ecde23b4bb0561146b9f8ce872de
Anujsingh9767 <singhanuj0990@gmail.com>
2025-04-15 16:25:41 +0530

    updated profile  implementation

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import express from "express"
 import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js"
+import cookieParser from "cookie-parser"
 
 const app =express()
 app.use(express.json())
@@ -12,6 +13,7 @@ dotenv.config()
 const PORT =process.env.PORT;
 
 app.use("/api/auth",authRoutes)
+app.use(cookieParser())
 //extract the json data
 
 const port =5001;
