357381bab8569a2b11bc6c434b879574e4ec13e3
Anujsingh9767 <singhanuj0990@gmail.com>
2025-04-16 22:23:35 +0530

    setting up axios for flobal state mangement protected the routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,10 @@ import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 const app =express()
-app.use(express.json())
-app.use(cookieParser())
+
 
 
 
@@ -24,7 +24,12 @@ dotenv.config()
 const PORT =process.env.PORT;
 
 
-
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 
 
