89a085ac4d3553cfe3ea7630433a0726e28cbd93
Anujsingh9767 <singhanuj0990@gmail.com>
2025-04-18 20:19:53 +0530

    compeleted messaging part

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -4,7 +4,7 @@ import { axiosInstance } from "../lib/axios"
 
 
 
-export const useChatStore = create ((set)=>({
+export const useChatStore = create ((set,get)=>({
     messages: [],
     users:[],
     selectedUser:null,
@@ -26,14 +26,24 @@ export const useChatStore = create ((set)=>({
     },
 
     getMessages: async(userId)=>{
-        set({isUserLoading:true});
+        set({ isMessageLoading:true});
         try {
             const res=await axiosInstance.get(`/message/${userId}`);
             set({messages:res.data});
         } catch (error) {
             toast.error(error.response.data.message);
         }finally{
-            set({isUserLoading: false});
+            set({isMessageLoading: false});
+        }
+    },
+    sendMessage: async(messageData)=>{
+        const {selectedUser,messages}=get()
+        try {
+            const res=await axiosInstance.post(`/message/send/${selectedUser._id}`,messageData);
+            set({messages:[...messages,res.data]});
+            //may be error
+        } catch (error) {
+            toast.error(error.response.data.message);
         }
     },
 
